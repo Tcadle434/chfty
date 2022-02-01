bc8234d8a4bd6f2cd430bac07c651d7d50d26ac3
Tcadle434 <thomascadle434@gmail.com>
2022-02-01 16:24:50 -0700

    compression

--- src/containers/homepage/topSection.jsx
+++ src/containers/homepage/topSection.jsx
@@ -1,10 +1,9 @@
-
-   
 import React from "react";
 import styled from "styled-components";
 import { Navbar } from "../../components/navbar";
 import Fade from 'react-reveal/Fade';
-import BackgroundImg from '../../assets/background.png'
+import BackgroundImg from '../../assets/background-min.png'
+
 
 const TopContainer = styled.div`
   width: 100%;
